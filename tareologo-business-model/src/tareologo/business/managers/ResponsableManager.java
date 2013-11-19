c6fb2b8c956bddc6b0d348e2e6063bfdbd4079ce
Alvaro Franco Gili <alvarogili@gmail.com>
2013-11-19 13:40:39 -0300

    Creada clase responsableManager, falta implementarla

---
+++ tareologo-business-model/src/tareologo/business/managers/ResponsableManager.java
@@ -0,0 +1,43 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package tareologo.business.managers;
+
+import java.util.List;
+import tareologo.business.model.Responsable;
+
+/**
+ *
+ * @author Alvaro Gili
+ */
+public class ResponsableManager extends BaseManager implements IBaseManager<Responsable>{
+
+    @Override
+    public void create(Responsable object) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Responsable retrieve(int objectID) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void update(Responsable object) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void delete(int objectID) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Responsable> getAll() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    
+}
